25400dccc64ebb03f4e2462ae96ed270a9c1805e
kdrkdrkdr <kdrhacker1234@gmail.com>
2023-11-20 23:25:20 +0900

    add files

--- test.py
+++ test.py
@@ -1,14 +1,5 @@
-
 from g2pk3 import G2p
-
 
 g2p = G2p()
 
-print(g2p('水をマレーシアから買わなくてはならないのです。hello It\'s is andrew.안녕하세요. The hat is being sold along with other Napoleonic memorabilia assembled by an industrialist who died last year.水をマレシアから買わなく. hello.'))
-# f = open('a.txt', 'r', encoding='utf-8').read().split('\n')
-# g = open('b.txt', 'w', encoding='utf-8')
-# for i in f:
-#     a = i.split('|')[1]
-#     g.write(g2p(a)+'\n')
-# g.close()
-
+print(g2p('水をマレーシアから買わなくてはならないのです。hello It\'s is andrew. 안녕하세요. The hat is being sold along with other Napoleonic memorabilia assembled by an industrialist who died last year.水をマレシアから買わなく. hello.'))
